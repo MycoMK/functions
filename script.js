77d1f75512b090a080191688256c1ab47bc2ba85
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-02-12 13:27:31 -0800

    learn-fucntions

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('connected')
+//  function declaration
+function great(name, lastName){
+console.log('Hi' + name + ' ' + lastName)
+}
+
+great('John', 'Smith');
+
+function people(firstName,lastName){
+   console.log(`Hey there ${firstName},${lastName}`)
+
+}
+people('John', 'Smith');
+people('Peter', 'Cech');
+people('Arnold', 'Luke');
+people('Emily', 'Button');
+
+//  function expression
+
+const speak = function(person1,person2){
+console.log(`Good morning  ${person1} and ${person2}`);
+};
+
+speak( 'Kings','Emily')
